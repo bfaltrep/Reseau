ff3948a4e935cbe62bfa4a33c1188a6210f6a384
Bérénice Faltrept <berenice.faltrept@gmail.com>
2014-11-07 16:06:26 +0100

    main gère paramètres vers client et serveur. variables de MThread sont plus construites

--- Projet/src/aquarium/Client.java
+++ Projet/src/aquarium/Client.java
@@ -9,9 +9,48 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 import aquarium.gui.Aquarium;
+import aquarium.gui.AquariumWindow;
 
-public class Client {
+public class Client extends Thread {
+	private Socket socket;
+	private BufferedReader in;
+	private PrintWriter out;
+	private Aquarium a ;
+	
+	public Client(){
+		//vide car initialisation dans le run
+	}
+	
+	public void run() {
+		try{
+			socket = new Socket (InetAddress.getLocalHost(),8888);
+			System.out.println("demande de connexion ");
+			
+			//MESSAGE DE BIENVENUE DANS L AQUARIUM
+			// réception > InputStream > ISReader > BufferedReader
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String message = in.readLine();
+			System.out.println(message);
+			
+			a = new Aquarium();
+			AquariumWindow animation = new AquariumWindow(a);
+			animation.displayOnscreen();
 
+			
+			//envoi d'un message
+			out = new PrintWriter(socket.getOutputStream());
+			out.println(" bonjour, merci de me transmettre l'aquarium");
+			out.flush();
+			
+			socket.close();
+		}catch(UnknownHostException e){
+			e.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
+		}		
+	}
+	
+	/*
 	public static void main(String[] args) {
 		Socket socket;
 		BufferedReader in;
@@ -29,30 +68,22 @@ public class Client {
 			String message = in.readLine();
 			System.out.println(message);
 			
+			a = new Aquarium();
+			AquariumWindow animation = new AquariumWindow(a);
+			animation.displayOnscreen();
+
 			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			/*
 			//envoi d'un message
 			out = new PrintWriter(socket.getOutputStream());
 			out.println(" bonjour, merci de me transmettre l'aquarium");
 			out.flush();
-			*/
+			
 			socket.close();
 		}catch(UnknownHostException e){
 			e.printStackTrace();
 		}catch(IOException e){
 			e.printStackTrace();
 		}
-	}
+	}*/
 
 }
