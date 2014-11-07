66f9c621687f618c3396872baf37679ae1488186
Marc-Alexandre Espiaut <marcalexandre@member.fsf.org>
2014-11-07 15:49:39 +0100

    refactoring protocole1.java

--- Projet/src/aquarium/Protocole1.java
+++ Projet/src/aquarium/Protocole1.java
@@ -9,57 +9,48 @@ public class Protocole1 {
 	//String = variable
 	//int = 4 byte
 	//Image ... je sais pas =='
-	
-	/*public static byte[] encoder (StableFish f){
-	//int
-	}*/
-	
+
 	//je vois pas comment transferer l'image
-	public static String encoderPoisson1Class (Image i, int width, int height, String nom, Point p){
-		return "";
+
+	public static byte[] encodeFishByte(int width, int height, String name, int x, int y) {
+		String result = width + "!" + height + "!" + x + "!" + y + "!" + name;
+		return result.getBytes();
 	}
 	
-	//poisson correspond à la position dans le tableau du client
-	public static String encoderPosition (int poisson, int x, int y){
-		return poisson+"!"+x+"!"+y;
-	}
-	
-	public static String encoderPoissonS (int width, int height, String nom, int x, int y){
-		return width+"!"+height+"!"+x+"!"+y+"!"+nom;
-	}
-	
-	public static byte[] encoderPoissonB (int width, int height, String nom, int x, int y){
-		String s = width+"!"+height+"!"+x+"!"+y+"!"+nom;
-		return s.getBytes();
+	public static String encodeFishString(int width, int height, String name, int x, int y) {
+		return width + "!" + height + "!" + x + "!" + y + "!" + name;
 	}
 
-	public static void decoderPoissonB(byte s[]) throws UnsupportedEncodingException{
-		String tmp1 = new String(s, "UTF-8");
-		String tmp[] = tmp1.split("!");
+	public static void decodeFishByte(byte input[]) throws UnsupportedEncodingException {
+		String rawInput = new String(input, "UTF-8");
+		String processedInput[] = rawInput.split("!");
 		int i = 0;
-		while(i < tmp.length){
-			System.out.println(tmp[i]);
-			i++;
-		}
-	}
-	
-	public static void decoderPoissonS(String s){
-		String tmp[] = s.split("!");
-		int i = 0;
-		while(i < tmp.length){
-			System.out.println(tmp[i]);
+		
+		while (i < processedInput.length) {
+			System.out.println(processedInput[i]);
 			i++;
 		}
 	}
 
-	public static void main(String[] args) throws UnsupportedEncodingException{
-		int a = 4;
-		int b = 5;
-		int c = 6;
-		int d = 7;
-		String s = "DorisFish";
-		byte tmp[] = encoderPoissonB(a,b,s,c,d);
-		decoderPoissonB (tmp);
+	public static void decodeFishString(String input) {
+		String processedInput[] = input.split("!");
+		int i = 0;
+		
+		while (i < processedInput.length) {
+			System.out.println(processedInput[i]);
+			i++;
+		}
 	}
 
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		int width = 4;
+		int height = 5;
+		int xPos = 6;
+		int yPos = 7;
+		String fishName = "DorisFish";
+		
+		byte tmp[] = encodeFishByte(width, height, fishName, xPos, yPos);
+
+		decodeFishByte(tmp);
+	}
 }
