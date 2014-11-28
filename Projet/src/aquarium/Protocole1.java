1cce15726355a5b0635b7d1b7c0c8b1e95c0bee4
Bérénice Faltrept <berenice.faltrept@gmail.com>
2014-11-28 16:57:07 +0100

    protocole

--- Projet/src/aquarium/Protocole1.java
+++ Projet/src/aquarium/Protocole1.java
@@ -2,64 +2,77 @@ package aquarium;
 
 import java.io.UnsupportedEncodingException;
 
+import aquarium.gui.Aquarium;
+import aquarium.items.Mobiles;
 import aquarium.util.Constante;
 
 public class Protocole1 {
-	// String Class nom, int, int, position Point(int int), Image,
-	// String = variable
-	// int = 4 byte
-	// Image ... je sais pas =='
 
-	// je vois pas comment transferer l'image
-
-	/*
-	 * 0: Déconnexion
-	 * 1: Connexion
-	 * 2: Créer poisson
-	 * 3: Tuer poisson
-	 * 4: Bouger poisson
-	 * 5: Nouvelle classe
-	 */
-
-	
-	//TODO: Se référer à Client.java pour savoir comment récupérer les variables de l'aquarium
-	public static byte[] encodeFishByte(int type, Aquarium aquarium) {
-		String result;
-		switch(type) {
-			case 0:
-				result = type + "!";
-				return result.getBytes();
-			case 1:
-				result = type + "!";
-				return result.getBytes();
-			case 2:
-				result = type + "!" + fishId + "!" + width + "!" + height + "!" + x + "!" + y;
-				return result.getBytes();
-			case 3:
-				result = type + "!" + fishId;
-				return result.getBytes();
-			case 4:
-				result = type + "!" + fishId + "!" + x + "!" + y;
-				return result.getBytes();
-			case 5:
-				result = type + "!";
-				return result.getBytes();
-			default:
-				return "".getBytes();
+	public static boolean decoder(String s,long idClient, Aquarium a){
+		String contenu [] = s.split("!");
+		String tmp = contenu[0];
+		
+		switch (tmp){
+		case "DECONNECT":
+			//message  : CMD
+			a.disconnectClient();
+			break;
+		case "ADDFISH":
+			//message  : CMD!idFish!width!height!x!y!StringClass
+			a.addFish(new Mobiles(idClient, 
+					Integer.parseInt(contenu[0]),
+					Integer.parseInt(contenu[1]), 
+					Integer.parseInt(contenu[2]), 
+					Integer.parseInt(contenu[3]), 
+					Integer.parseInt(contenu[4]), 
+					a.getClasse(a.getClasseIndex(contenu[5])).getImages()));
+			break;
+		case "REMOVEFISH":
+			//message  : CMD!idFish
+			a.removeFish(idClient,contenu[1]);
+			break;
+		case "MOVEFISH":
+			//message  : CMD!idFish!x!y
+			a.modifySingleClientObj(idClient,
+					Integer.parseInt(contenu[0]),
+					Integer.parseInt(contenu[1]),
+					Integer.parseInt(contenu[2]));
+			break;
+		case "NEWCLASS":
+			//message  : CMD!nomClasse
+			a.addClasses(idClient,contenu[1],"image/polochon.jpg");
+			break;
+		default: 
+			System.out.println("Reception d'un message non conforme. ");
+			System.out.println("non traité : "+s);
+			break;
 		}
+	
+		return true;
 	}
-	// !! \\
-	public static String encodeFishString(int id, int width, int height, int x,
-			int y, String name) {
-		return id + "!" + width + "!" + height + "!" + x + "!" + y + "!" + name;
+	
+	public static String encodeDisconnect(){
+		return "DECONNECT";
+	}
+	
+	public static String encodeNewFish(int idFish, int width, int height, int x, int y, String nameClass) {
+		return "ADDFISH"+"!"+idFish + "!" + width + "!" + height + "!" + x + "!" + y + "!" + nameClass;
+	}
+	
+	public static String encodeKillFish(int idFish){
+		return "REMOVEFISH"+"!"+idFish;
+	}
+	
+	public static String encodeMoveFish(int idFish, int x, int y){
+		return "MOVEFISH"+"!"+idFish+"!"+x+"!"+y;
 	}
 
-	/**
-	 * @param input
-	 * @throws UnsupportedEncodingException
-	 */
-	public static void decodeFishByte(byte input[])
-			throws UnsupportedEncodingException {
+	public static String encodeNewClass(String nomClass){
+		return "NEWCLASS"+"!"+nomClass;
+	}
+	
+/* traitement par byte[]
+	public static void decodeFishByte(byte input[])	throws UnsupportedEncodingException {
 		String rawInput = new String(input, "UTF-8");
 		String processedInput[] = rawInput.split("!");
 		int i = 0;
@@ -68,33 +81,6 @@ public class Protocole1 {
 			System.out.println(processedInput[i]);
 			i++;
 		}
-	}
+	}*/
 
-	public static void decodeFishString(String input) {
-		String processedInput[] = input.split("!");
-		int i = 0;
-
-		while (i < processedInput.length) {
-			System.out.println(processedInput[i]);
-			i++;
-		}
-	}
-
-	public static int decode(String input) {
-		return 0;
-	}
-
-	public static void main(String[] args) throws UnsupportedEncodingException {
-		int width = 4;
-		int height = 5;
-		int xPos = 6;
-		int yPos = 7;
-		String fishName = "DorisFish";
-		System.out.println(width + height + xPos + yPos + fishName); // retirer
-																		// les
-																		// warnings
-
-		// byte tmp[] = encodeFishByte(width, height, fishName, xPos, yPos);
-		// decodeFishByte(tmp);
-	}
 }
